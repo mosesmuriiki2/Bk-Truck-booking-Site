95ab96d36dcaecae6886c9db6071ec626a751606
Ngicu Moses <ngicumoses@gmail.com>
2025-12-09 18:57:09 +0300

    error fixes

--- api/index.py
+++ api/index.py
@@ -1,11 +1,5 @@
 import os
-from pathlib import Path
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bkbookingtrucks.settings')
 
-# Expose ASGI application for Vercel Python runtime
 from django.core.asgi import get_asgi_application
 app = get_asgi_application()
-
-# Vercel's @vercel/python will import `application` from this file
-# and serve it for each request as a serverless function.
